f796c7184bc4ae7b32b4f97a4f61d21b03b46190
swat1508 <swatantrasinha1508@gmail.com>
2019-02-18 22:20:02 +0530

    "trying configuration in database.js"

--- config/database.js
+++ config/database.js
@@ -5,5 +5,14 @@
 //   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 // }
 
-
+/*
 module.exports = {mongoURI: 'mongodb://swat1508:sinha1508@ds129045.mlab.com:29045/vidjot-prod'};
+*/
+
+if(process.env.NODE_ENV == 'production'){
+console.log('if => process.env.NODE_ENV ==> ', process.env.NODE_ENV);
+  module.exports = {mongoURI: 'mongodb://swat1508:sinha1508@ds129045.mlab.com:29045/vidjot-prod'};
+}else{
+  console.log('else=> process.env.NODE_ENV ==> ', process.env.NODE_ENV);
+  module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
+}
